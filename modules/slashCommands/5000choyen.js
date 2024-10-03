888833ff6aa024f91ba0bdd0f693a9ca55b34e6e
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-03 14:26:13 +0900

    Create 5000choyen.js

---
+++ modules/slashCommands/5000choyen.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const cooldown = require('../events/cooldown');
+const slashCommandError = require('../errors/slashCommandError');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('5000choyen')
+    .setDescription('5000兆円欲しい!!風の画像生成')
+    .addStringOption(option =>
+      option.setName('top')
+        .setDescription('上部文字列')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('bottom')
+        .setDescription('下部文字列')
+        .setRequired(true)),
+
+  async execute(interaction) {
+    try {
+      const commandName = this.data.name;
+      const isCooldown = cooldown(commandName, interaction);
+      if (isCooldown) return;
+
+      await interaction.deferReply();
+      const top = interaction.options.getString('top');
+      const bottom = interaction.options.getString('bottom');
+
+      const embed = new EmbedBuilder()
+        .setColor('#f8b4cb')
+        .setFooter({ text:'Emubot | 5000choyen', iconURL: interaction.client.user.displayAvatarURL() })
+        .setTimestamp()
+        .setImage(`https://gsapi.cbrx.io/image?top=${encodeURIComponent(top)}&bottom=${encodeURIComponent(bottom)}&type=png`);
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      slashCommandError(interaction.client, interaction, error);
+    }
+  },
+};
