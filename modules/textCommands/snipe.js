ec04d77f229d56ed33be658c75591d4d05bf9714
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-03 10:53:36 +0900

    Update snipe.js

--- modules/textCommands/snipe.js
+++ modules/textCommands/snipe.js
@@ -1,1 +1,33 @@
+const { EmbedBuilder, Events } = require('discord.js');
+const { loadSnipeData } = require('../../lib/snipe'); 
 
+module.exports = {
+    name: Events.MessageCreate,
+    async execute(message, client) {
+        if (message.content.startsWith('!snipe')) {
+            try {
+                const snipeData = loadSnipeData();
+                const channelId = message.channel.id;
+
+                if (snipeData[channelId]) {
+                    const snipe = snipeData[channelId];
+
+                    const embed = new EmbedBuilder()
+                        .setColor('#f8b4cb')
+                        .setTitle(`${snipe.author.displayName}のメッセージ`)
+                        .setDescription(snipe.content || '内容はありません')
+                        .setThumbnail(snipe.author.avatarURL) 
+                        .setFooter({ text: 'Emubot | snipe', iconURL: client.user.displayAvatarURL() })
+                        .setTimestamp(new Date(snipe.timestamp * 1000));
+
+                    await message.channel.send({ embeds: [embed] });
+                } else {
+                    await message.channel.send('このチャンネルには削除されたメッセージはありません。');
+                }
+            } catch (error) {
+                console.error('エラーが発生しました:', error);
+                await message.channel.send('メッセージを取得する際にエラーが発生しました。');
+            }
+        }
+    }
+};
