31bb10b28dba3460d31007ec7d5ed11d89d9c4de
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-07 15:22:45 +0900

    Create timer.js

---
+++ modules/slashCommands/timer.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require('discord.js');
+const slashCommandError = require('../errors/slashCommandError');
+const cooldown = require('../events/cooldown');
+const { saveTimer, startTimer, validateTime } = require('../../lib/timer');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('timer')
+    .setDescription('指定した時間後に通知するタイマーを起動します。')
+    .addIntegerOption(option =>
+      option.setName('minutes')
+        .setDescription('分を指定してください。')
+        .setRequired(true)
+        .setMinValue(0) 
+        .setMaxValue(60) 
+    )
+    .addIntegerOption(option =>
+      option.setName('seconds')
+        .setDescription('秒を指定してください。')
+        .setRequired(false)
+        .setMinValue(0) 
+        .setMaxValue(60) 
+    ),
+
+  async execute(interaction) {
+    try {
+      const commandName = this.data.name;
+      const isCooldown = cooldown(commandName, interaction);
+      if (isCooldown) return;
+
+      const minutes = interaction.options.getInteger('minutes');
+      const seconds = interaction.options.getInteger('seconds') || 0;
+
+      const totalSeconds = (minutes * 60) + seconds;
+
+      await interaction.reply(`タイマーを ${minutes} 分 ${seconds} 秒で起動します。`);
+      saveTimer(interaction.user.id, interaction.channel.id, totalSeconds);
+      startTimer(interaction, minutes, seconds, totalSeconds);
+    } catch (error) {
+      slashCommandError(interaction.client, interaction, error);
+    }
+  },
+};
