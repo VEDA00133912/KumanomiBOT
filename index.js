706a1306c7eefb95d646cfcc346482ba965cceed
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-23 13:14:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const setupGlobalErrorHandling = require('./modules/errors/globalError');
-const { loadCommands, loadTextCommands, loadEvents } = require('./lib/loader');
+const { loadCommands, loadTextCommands, loadAdminCommands, loadEvents } = require('./lib/loader');
 const path = require('path');
 const { config } = require('dotenv');
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessagePolls] });
 
 client.commands = new Collection();
 client.contextMenuCommands = new Collection();
@@ -13,7 +12,7 @@ client.contextMenuCommands = new Collection();
 loadCommands(client, path.join(__dirname, 'modules/slashCommands'));
 loadCommands(client, path.join(__dirname, 'modules/contextMenus'));
 loadTextCommands(client, path.join(__dirname, 'modules/textCommands'));
+loadAdminCommands(client, path.join(__dirname, 'modules/adminCommands'));
 loadEvents(client, path.join(__dirname, 'modules/events'));
 
-setupGlobalErrorHandling(client);
 client.login(process.env.TOKEN);
