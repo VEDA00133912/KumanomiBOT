278340db3c30a1557d7eac4ec2392e5effcbabda
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-23 15:16:30 +0900

    Add files via upload

---
+++ modules/contextMenus/translate.js
@@ -0,0 +1,34 @@
+const { ApplicationCommandType, EmbedBuilder, ContextMenuCommandBuilder } = require('discord.js');
+const { translater } = require('../../lib/translate');  
+const { validateMessageContent } = require('../../lib/invalidContent'); 
+const contextMenuError = require('../errors/contextMenuError');
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('英語に翻訳')
+    .setType(ApplicationCommandType.Message),
+
+  async execute(interaction) {
+
+    const text = interaction.targetMessage.content; 
+
+    const hasInvalidContent = await validateMessageContent(interaction, text);  
+    if (hasInvalidContent) return; 
+
+    try {
+　    await interaction.deferReply();  
+      const targetLanguage = 'en';  
+      const translatedText = await translater(text, 'ja', targetLanguage);  
+
+      const embed = new EmbedBuilder()
+        .setDescription('**翻訳しました！**' + '\n' + '```\n' + `${translatedText}` + '\n```')
+        .setTimestamp()
+        .setFooter({ text: 'Emubot | translate', iconURL: interaction.client.user.displayAvatarURL() })
+        .setColor('#f8b4cb');
+
+      await interaction.editReply({ embeds: [embed] });  
+    } catch (error) {
+        contextMenuError(interaction.client, interaction, error);
+    }
+  },
+};
