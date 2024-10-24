440fd4b22d1a19b173570acfe78227252ccd688e
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-24 11:24:37 +0900

    Update interactionError.js

--- modules/errors/interactionError.js
+++ modules/errors/interactionError.js
@@ -14,7 +14,7 @@ module.exports = (client, interaction, error) => {
             { name: 'Server', value: interaction.guild ? interaction.guild.name : 'N/A', inline: true },
             { name: 'Time', value: new Date().toLocaleString(), inline: true }
         )
-        .setFooter({ text: `Emubot | ${interaction.commandName}`, iconURL: client.user.displayAvatarURL() });
+        .setFooter({ text: `Kumanomi | ${interaction.commandName}`, iconURL: client.user.displayAvatarURL() });
 
     const errorChannel = client.channels.cache.get(channelId);
     if (errorChannel) {
