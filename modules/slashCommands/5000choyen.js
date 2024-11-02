05e9c1cc8485d27cc89f40c4a657b0086caf583a
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-02 15:23:55 +0900

    Update 5000choyen.js

--- modules/slashCommands/5000choyen.js
+++ modules/slashCommands/5000choyen.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const cooldown = require('../events/cooldown');
 const slashCommandError = require('../errors/slashCommandError');
 const { createEmbed } = require('../../lib/embed');
