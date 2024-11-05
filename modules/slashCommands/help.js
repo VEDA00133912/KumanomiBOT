ed984a4b4af5acf730e8a53ba8ae99aa2a1d3047
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-05 12:59:32 +0900

    Update help.js

--- modules/slashCommands/help.js
+++ modules/slashCommands/help.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const buttonPages = require("../../lib/pagination");
 const cooldown = require('../events/cooldown');
+const buttonPages = require('../../lib/pagination');
 const { createEmbed } = require('../../lib/embed');
 
 module.exports = {
        data: new SlashCommandBuilder()
               .setName('help')
-              .setDescription('くまのみぼっとのヘルプを表示します。'),
+              .setDescription('くまのみBOTのヘルプを表示します。'),
 
        async execute(interaction) {
               const commandName = this.data.name;
@@ -17,13 +17,12 @@ module.exports = {
               const commandMap = new Map(commands.map(cmd => [cmd.name, cmd.id]));
 
                const embed1 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと｜help**')
+                   .setDescription('**くまのみぼっと｜help (⚙)BOT概要**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
                      { name: 'くまのみぼっとについて', value: '暇な音ゲーマーの作ってる多機能botです' },
                      { name: '新機能のおしらせチャンネルについて', value: 'サポートサーバーのアナウンスチャンネルをフォローすることでおしらせを受け取れます' },
                      { name: 'helpの操作方法', value: 'ボタンを押すことでコマンド一覧等が見れます' },
-                     { name: 'Make it a Quote機能', value: 'メッセージの返信のときにBOTをメンションするとMake it a Quote画像を生成します' },
                      { name: 'サイト', value: '[くまのみぼっと公式サイト](https://veda00133912.github.io/kumanomi-site/)' },
                      { name: 'サポート等', value: '<:twitter:1282701797353459799> [twitter](https://twitter.com/ryo_001339)  <:discord:1282701795000320082> [Discord](https://discord.gg/j2gM7d2Drp)  <:github:1282850416085827584> [Github](https://github.com/VEDA00133912/KumanomiBOT)' },
                      { name: '制作者', value: '<@1095869643106828289> (ryo_001339)' }
@@ -35,67 +34,67 @@ module.exports = {
                };
 
                const embed2 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 1]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
                        getCommandField('help', 'helpメッセージを表示するコマンド'),
-                       { name: `</select taiko:1298846149926715458>`, value: '太鼓の達人ランダム選曲コマンド(全曲、★10)' },
-                       { name: `</select prsk:1298846149926715458>`, value: 'プロセカランダム選曲コマンド(MASTER、APPEND)' },
-                       { name: `</select chunithm:1298846149926715458>`, value: 'CHUNITHMランダム選曲コマンド(全曲、ORIGINAL、WE&ULTIMA)' },
-                       { name: `</select maimai:1298846149926715458>`, value: 'maimaiランダム選曲コマンド(全曲、maimai、宴譜面)' },
-                       { name: `</select ongeki:1298846149926715458>`, value: 'オンゲキランダム選曲コマンド(全曲、オンゲキ、LUNATIC、Re:MASTER)' },
+                       { name: '</select taiko:1298846149926715458>', value: '太鼓の達人ランダム選曲コマンド(全曲、★10)' },
+                       { name: '</select prsk:1298846149926715458>', value: 'プロセカランダム選曲コマンド(MASTER、APPEND)' },
+                       { name: '</select chunithm:1298846149926715458>', value: 'CHUNITHMランダム選曲コマンド(全曲、ORIGINAL、WE&ULTIMA)' },
+                       { name: '</select maimai:1298846149926715458>', value: 'maimaiランダム選曲コマンド(全曲、maimai、宴譜面、Re:MASTER)' },
+                       { name: '</select ongeki:1298846149926715458>', value: 'オンゲキランダム選曲コマンド(全曲、オンゲキ、LUNATIC、Re:MASTER)' },
                    );
 
                const embed3 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 2]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
-                       { name: `</info server:1298846149486182519>`, value: 'サーバー情報を表示するコマンド' },
-                       { name: `</info user:1298846149486182519>`, value: 'ユーザー情報を表示するコマンド' },                
+                       { name: '</info server:1298846149486182519>', value: 'サーバー情報を表示するコマンド' },
+                       { name: '</info user:1298846149486182519>', value: 'ユーザー情報を表示するコマンド' },         
+                    　 { name: '</info system:1298846149486182519>', value: 'BOTのシステム情報を表示するコマンド' },                
                        getCommandField('spoofing', '他ユーザーに自由になりすましできるコマンド'),
                        getCommandField('5000choyen', '5000兆円欲しい！画像生成コマンド'),
-                       getCommandField('omikuji', 'おみくじを引けるコマンド'),
-                       getCommandField('timer', 'タイマーコマンド')
+                       getCommandField('omikuji', 'おみくじを引けるコマンド')
                    );
 
               const embed4 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 3]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
-                       { name: `</create role:1298846149486182514>`, value: 'ロール作成コマンド' },                
-                       { name: `</create channel:1298846149486182514>`, value: 'チャンネル作成コマンド' },                
-                       { name: `</create password:1298846149486182514>`, value: 'パスワード生成コマンド' },                
+                       { name: '</create role:1298846149486182514>', value: 'ロール作成コマンド' },                
+                       { name: '</create channel:1298846149486182514>', value: 'チャンネル作成コマンド' },                
+                       { name: '</create emoji:1298846149486182514>', value: '絵文字作成コマンド' },                
                        getCommandField('timer', 'タイマーコマンド'),
                        getCommandField('totsu-shi', '突然の死ジェネレーターコマンド'),
                        getCommandField('icon', '指定したユーザーのアイコン画像を表示するコマンド'),
                    );
 
               const embed5 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 4]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
-                       { name: `</fake nitro:1298846149486182517>`, value: '偽Nitro生成コマンド(Gift, Promo)' },                
-                       { name: `</fake token:1298846149486182517>`, value: '偽Token生成コマンド' }, 
-                       getCommandField('convert', 'メッセージを変換するコマンド\n(ルーン文字、フェニキア文字、ヒエログリフ、逆読み、アナグラム、メンヘラ文、怪しい日本語)'),
+                       { name: '</fake nitro:1298846149486182517>', value: '偽Nitro生成コマンド(Gift, Promo)' },                
+                       { name: '</fake token:1298846149486182517>', value: '偽Token生成コマンド' }, 
+                       getCommandField('backword', '逆読みコマンド'),
                        getCommandField('qr', 'URLをQRコードに変換するコマンド'),
                        getCommandField('ping', 'BOTのping値を表示するコマンド'),
                        getCommandField('delete', 'メッセージ削除コマンド') 
                    );
 
               const embed6 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 5]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
-                       { name: `</url short:1298846150069190697>`, value: 'URL短縮コマンド' }, 
-                       { name: `</url extract:1298846150069190697>`, value: 'URL抽出コマンド' }, 
-                       { name: `</url viruscheck:1298846150069190697>`, value: 'URL危険性チェックコマンド' }, 
+                       { name: '</url short:1298846150069190697>', value: 'URL短縮コマンド' }, 
+                       { name: '</url extract:1298846150069190697>', value: 'URL抽出コマンド' }, 
+                       { name: '</url viruscheck:1298846150069190697>', value: 'URL危険性チェックコマンド' }, 
                        getCommandField('prime', '素数判定を行うコマンド'),
                        getCommandField('translate', 'Google翻訳コマンド(日本語→英語、韓国語、中国語、ロシア語)'),
                        getCommandField('yahoonews', 'yahooニュースリンクを取得するコマンド')
                    );
                    
               const embed7 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 6]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
                        getCommandField('ticket', 'チケットコマンド(プライベートチャンネルの作成)'),
@@ -107,33 +106,46 @@ module.exports = {
                    );
                      
               const embed8 = createEmbed(interaction)
-                   .setDescription('**くまのみぼっと | help [スラッシュコマンド一覧 7]**')
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
                        getCommandField('hiroyuki', 'ひろゆきボイスのmp3に変換'),
                        getCommandField('slowmode', '低速モードの設定'),
                        getCommandField('hash', 'ハッシュ化コマンド'),
-                       getCommandField('password', 'パスワード生成コマンド')
+                       getCommandField('mcstatus', 'マイクラサーバーステータス表示コマンド'),
+                       getCommandField('password', 'パスワード生成コマンド'),
+                       getCommandField('timer', 'タイマーコマンド')
+
                    );
 
-               const embed9 = new EmbedBuilder()
-                   .setDescription('**くまのみぼっと | help [コンテキストメニューコマンド一覧**')
+              const embed9 = createEmbed(interaction)
+                   .setDescription('**くまのみぼっと | help (/)スラッシュコマンド**')
+                   .setThumbnail(interaction.client.user.displayAvatarURL())
+                   .addFields(
+                       getCommandField('rune', 'ルーン文字変換コマンド'),
+                       getCommandField('cjp', '怪しい日本語変換コマンド'),
+                       getCommandField('menhera', 'メンヘラ文変換コマンド'),
+                       getCommandField('anagram', 'アナグラムコマンド')
+                   );
+           
+               const embed10 = new EmbedBuilder()
+                   .setDescription('**くまのみぼっと | help コンテキストメニューコマンド**')
                    .setFooter({ text: 'Kumanomi | help', iconURL: interaction.client.user.displayAvatarURL() })
                    .setThumbnail(interaction.client.user.displayAvatarURL())
                    .addFields(
-                    { name: '**アイコン表示**', value: 'アイコンを表示するuserコンテキストメニューコマンド' },
-                    { name: '**英語に翻訳**', value: '英語に翻訳するmessageコンテキストメニューコマンド' },
-                    { name: '**突然の死ジェネレーター**', value: '突然の死messageコンテキストメニューコマンド' },
-                    { name: '**ひろゆき変換**', value: 'ひろゆきボイスのmp3に変換するコンテキストメニューコマンド' },
-                    { name: '**Make it a Quote(カラー)**', value: 'カラーでmiq生成するコンテキストメニューコマンド' },
-                    { name: '**Make it a Quote(モノクロ)**', value: 'モノクロでmiq生成するコンテキストメニューコマンド' }
+                    { name: '**アイコン表示**', value: 'ユーザーのアイコンを表示するコマンド' },
+                    { name: '**英語に翻訳**', value: 'メッセージを英語に翻訳するコマンド' },
+                    { name: '**突然の死ジェネレーター**', value: '突然の死ジェネレーターコマンド' },
+                    { name: '**ひろゆき変換**', value: 'メッセージをひろゆきのmp3に変換するコマンド' },
+                    { name: '**Make it a Quote(カラー)**', value: 'カラーでMake it a Quoteを生成するコマンド' },
+                    { name: '**Make it a Quote(モノクロ)**', value: 'モノクロでMake it a Quoteするコマンド' }
                 )
                 .setTimestamp()
                 .setColor('Green');
 
 
-              const embed10 = new EmbedBuilder()
-                .setDescription('**くまのみぼっと | help [テキストコマンド一覧]**')
+              const embed11 = new EmbedBuilder()
+                .setDescription('**くまのみぼっと | help (^)テキストコマンド**')
                 .setFooter({ text: 'Kumanomi | help', iconURL: interaction.client.user.displayAvatarURL() })
                 .setThumbnail(interaction.client.user.displayAvatarURL())
                 .addFields(
@@ -143,7 +155,7 @@ module.exports = {
                 .setTimestamp()
                 .setColor('Blue');
 
-        const pages = [embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8, embed9, embed10];
+        const pages = [embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8, embed9, embed10, embed11];
             await buttonPages(interaction, pages);
     },
 };
