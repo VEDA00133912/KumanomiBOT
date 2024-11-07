254fe4a26add41cbd682d201ef10201fa73c3e63
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-07 13:15:09 +0900

    Add files via upload

---
+++ modules/contextmenus/translate.js
@@ -0,0 +1,38 @@
+const { ApplicationCommandType, EmbedBuilder, ContextMenuCommandBuilder } = require('discord.js');
+const { translater } = require('../../lib/translate');  
+const { validateMessageContent } = require('../../lib/invalidContent'); 
+const contextMenuError = require('../errors/contextmenuError');
+const cooldown = require('../events/cooldown');
+const { createEmbed } = require('../../lib/embed');
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('英語に翻訳')
+    .setType(ApplicationCommandType.Message)
+    .setContexts(0,1,2)
+    .setIntegrationTypes(0,1),
+
+  async execute(interaction) {
+    const commandName = this.data.name;
+    if (cooldown(commandName, interaction)) return;
+
+      
+    const text = interaction.targetMessage.content; 
+
+    const hasInvalidContent = await validateMessageContent(interaction, text);  
+    if (hasInvalidContent) return; 
+
+    try {
+　    await interaction.deferReply();  
+      const targetLanguage = 'en';  
+      const translatedText = await translater(text, 'ja', targetLanguage);  
+
+      const embed = createEmbed(interaction)
+        .setDescription('**翻訳しました！**' + '\n' + '```\n' + `${translatedText}` + '\n```');
+
+      await interaction.editReply({ embeds: [embed] });  
+    } catch (error) {
+        contextMenuError(interaction.client, interaction, error);
+    }
+  },
+};
