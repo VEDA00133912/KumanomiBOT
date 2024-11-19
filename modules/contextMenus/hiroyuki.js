6e3f5360451bae45a18ff0ec22d3e3c8b03c8081
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-11-19 13:48:46 +0900

    Add files via upload

---
+++ modules/contextMenus/hiroyuki.js
@@ -0,0 +1,39 @@
+const { ApplicationCommandType, ContextMenuCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { generateAudio, deleteAudioFile } = require('../../lib/hiroyuki');
+const { checkPermissions } = require('../../lib/permissions'); 
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('ひろゆきのMP3に変換')
+    .setType(ApplicationCommandType.Message)
+    .setContexts(0,1,2)
+    .setIntegrationTypes(0,1),
+
+  async execute(interaction) {
+        const requiredPermissions = [ PermissionFlagsBits.SendMessages, PermissionFlagsBits.AttachFiles ];
+        const isMissingPermissions = await checkPermissions(interaction, requiredPermissions);
+        if (isMissingPermissions) return;
+      
+    const messageContent = interaction.targetMessage.content;
+
+    if (messageContent.length > 1000) {
+      await interaction.reply({ content: '<:error:1302169165905526805>1000字以下にしてください。', ephemeral: true });
+      return; 
+    }
+
+    await interaction.reply('<a:loading:1302169108888162334> 生成中...');
+
+    try {
+      const audioFilePath = await generateAudio(messageContent, interaction);
+      if (audioFilePath) {
+        await interaction.editReply({ content: '<:check:1302169183110565958> 生成完了！', files: [audioFilePath] });
+        await deleteAudioFile(audioFilePath);
+      }
+    } catch (error) {
+      await interaction.editReply(error.message);
+      setTimeout(async () => {
+        await interaction.deleteReply().catch(console.error); 
+      }, 5000);
+    }
+  },
+};
--- modules/contextMenus/icon.js
+++ modules/contextMenus/icon.js
@@ -1,1 +1,37 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const cooldown = require('../events/cooldown');
+const contextMenuError = require('../errors/contextMenuError');
+const { createEmbed } = require('../../lib/embed');
 
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('アイコン表示')
+    .setType(ApplicationCommandType.User)
+    .setContexts(0,1,2)
+    .setIntegrationTypes(0,1),
+
+  async execute(interaction) {
+    const commandName = this.data.name;
+    const isCooldown = cooldown(commandName, interaction);
+    if (isCooldown) return;
+
+    await interaction.deferReply();
+
+    const targetUser = interaction.targetUser;
+
+    try {
+      const member = await interaction.guild?.members.fetch(targetUser.id).catch(() => null);
+      const avatarURL = member 
+        ? member.displayAvatarURL({ size: 1024 }) 
+        : targetUser.displayAvatarURL({ size: 1024 });
+
+      const embed = createEmbed(interaction)
+        .setDescription(`<@${targetUser.id}>**[のアイコン](${avatarURL})**`)
+        .setImage(avatarURL);
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      contextMenuError(interaction.client, interaction, error);
+    }
+  },
+};
