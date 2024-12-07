b4016e993d0d9a4f3af769ce9eacc19c2b77aefc
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-07 22:58:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const path = require('path');
 const { config } = require('dotenv');
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences ] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers
+/*, GatewayIntentBits.GuildPresences*/ ] });
 
 client.commands = new Collection();
 client.contextMenuCommands = new Collection();
 
-loadCommands(client, path.join(__dirname, 'modules/slashcommands'));
-loadCommands(client, path.join(__dirname, 'modules/contextmenus'));
-loadTextCommands(client, path.join(__dirname, 'modules/textcommands'));
-loadAdminCommands(client, path.join(__dirname, 'modules/admin'));
+loadCommands(client, path.join(__dirname, 'modules/slashCommands'));
+loadCommands(client, path.join(__dirname, 'modules/contextMenus'));
+loadTextCommands(client, path.join(__dirname, 'modules/textCommands'));
+loadAdminCommands(client, path.join(__dirname, 'modules/adminCommands'));
 loadEvents(client, path.join(__dirname, 'modules/events'));
 
 setupGlobalErrorHandling(client);
